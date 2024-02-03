649aed4a8f377c972ae93b97b6b097d3424e6d16
sdunlap1 <sdunlap1@mac.com>
2024-02-02 17:30:21 -0800

    Adding Obj Enhance for first commit

---
+++ my.js
@@ -0,0 +1,43 @@
+// Writing a ES2015 Version
+
+// Same keys and values
+function createInstructor(firstName, lastName) {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+//Computed Property Names
+let favoriteNumber = 8;
+const instructor = {
+  fisrtName: "Steve",
+  [favoriteNumber]: "Is my favy fav",
+};
+
+//Object Methods
+const instructors = {
+  firstName: "Steve",
+  sayHi() {
+    return "Hi";
+  },
+  sayBye() {
+    return this.firstName + "" + " says bye!";
+  },
+};
+
+//createAnimal function
+const d = createAnimal("dog", "bark", "Woooof!");
+d.bark();
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa");
+s.bleet()
+
+function createAnimal(species, verb, noise) {
+  return {
+    species,
+    [verb]() {
+      return noise;
+    },
+  };
+}
